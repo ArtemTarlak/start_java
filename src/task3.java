692088130e95f76d2c7d252957c3742835225ce0
ArtemTarlak <tarlokian.0106@gmail.com>
2024-11-19 10:38:28 +0300

    Решение 3 задачи

--- src/task3.java
+++ src/task3.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class task3 {
+    public static  void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = sc.nextLine();
+        String stroka = "";
+        for (int i = str.length()-1; i >= 0; i--){
+            stroka += str.charAt(i);
+        }
+        System.out.println(stroka);
+    }
 }
